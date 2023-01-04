0092cdfd0e7c6b535f4d2b54c42f0f697a76993f
Samuel Martino <119631472+onitram52@users.noreply.github.com>
2023-01-04 16:35:39 -0500

    Add files via upload

---
+++ my_first_module.py
@@ -0,0 +1,11 @@
+def ravioli_ravioli():
+    length = float(input("What is the length of you house in feet?: "))
+    width = float(input("What is the width of your house in feet?: "))
+    print("Your house's area is: ", (length * width), "square feet.")
+
+
+from math import pi
+
+def formuoli():
+    radius = float(input("What is the radius of this circle in inches?: "))
+    print("The circumference of this circle is: ", (2 * pi * radius), "inches.")
